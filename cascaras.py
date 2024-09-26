2fb659f661048ff961e952f0bbf39d8b3462ef2b
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-25 20:41:47 -0600

    Add files via upload

---
+++ cascaras.py
@@ -0,0 +1,44 @@
+print("Roman De la Cruz Leonardo Antonio, listas")
+thislist = ["carro", "moto", "avion"]#las variables
+print(thislist)#imprime la lista
+
+#cuando usamos (len( )) te despliega
+#el numero de elementos de la lista
+thislist2 = ["carro", "moto", "avion"]
+print(len(thislist))
+#Separa con un espacio
+print(" ")
+#Da valores a varias listas
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+list4 = ["abc", 34, True, 40, "male"]
+
+#Muestra los miembros de cada lista
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+
+
+print(" ")
+
+thislist = ["apple", "banana", "cherry"]
+
+#Muestra el miembro 1 y comienza en Cero 0
+print(thislist[1])
+
+#Muestra el ultimo miembro de la lisa
+print(thislist[-1])
+#muestra las lineas con las diferentes variables
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])#imprime en orden de los numeros en la lista
+
+thislist = ["0", "1", "2", "3", "4", "5", "6"]
+print(thislist[2:5])
+
+thislist = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "K"]
+print(thislist[2:])
+
+thislist = ["moto", "carro", "barco", "avion", "lancha", "yate", "helicoptero"]
+print(thislist[-3*2]) 
